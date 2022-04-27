50eb5c4eb5fd17172f7048b51b10bb6bd27ff7b8
Jayhom <48545313+Jayhom-Jiang@users.noreply.github.com>
2022-04-27 15:53:19 +0800

    Delete chapter10 directory

--- chapter10/10-11-read.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('favorite_number.json') as f:
-    number = json.load(f)
-
-print("I know your favorite number! It's " + str(number) + ".")
--- chapter10/10-3.py
+++
@@ -1,6 +0,0 @@
-name = input("What's your name? ")
-
-filename = 'guest.txt'
-
-with open(filename, 'w') as f:
-    f.write(name)
