9299f89ef1a982c23368322a9939b71c0d625af2
Jayhom <48545313+Jayhom-Jiang@users.noreply.github.com>
2022-04-27 15:48:33 +0800

    Add files via upload

---
+++ chapter07/7-4.py
@@ -0,0 +1,11 @@
+prompt = "\nTell me Pizza Toppings"
+prompt += "\nEnter 'quit' to end the program."
+
+active = True
+while active:
+    message = raw_input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print('Add ' + message + ".")
